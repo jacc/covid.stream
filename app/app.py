7a1e3355490d8332d518f852cabf7b30252e2a02
Jack LaFond <jack.lafond@icloud.com>
2020-03-14 23:14:18 -0400

    add /numbers/confirmed,deaths,recovered

--- app/app.py
+++ app/app.py
@@ -79,8 +79,82 @@ class LatestNumbers(object):
                 "totalRecoveredNumbers": totalRecoveredNumbers,
             }
         }
+        
+class LatestConfirmed(object):
+    def __init__(self, redis_connection):
+        self._redis = redis_connection
 
+    def on_get(self, req, resp):
+        with self._redis.pipeline() as pipe:
+            pipe.get("totalConfirmedNumbers")
+            results = pipe.execute()
 
+        for item in results:
+            try:
+                check_int = int(item)
+            except Exception:
+                logger.info("invalid information.")
+                totalConfirmedNumbers = None
+                break
+
+            totalConfirmedNumbers = int(results[0])
+
+        resp.media = {
+            "data": {
+                "totalConfirmedNumbers": totalConfirmedNumbers
+            }
+        }
+        
+class LatestDeaths(object):
+    def __init__(self, redis_connection):
+        self._redis = redis_connection
+
+    def on_get(self, req, resp):
+        with self._redis.pipeline() as pipe:
+            pipe.get("totalDeathNumbers")
+            results = pipe.execute()
+
+        for item in results:
+            try:
+                check_int = int(item)
+            except Exception:
+                logger.info("invalid information.")
+                totalDeathNumbers = None
+                break
+
+            totalDeathNumbers = int(results[0])
+
+        resp.media = {
+            "data": {
+                "totalDeathNumbers": totalDeathNumbers
+            }
+        }
+ 
+class LatestRecovered(object):
+    def __init__(self, redis_connection):
+        self._redis = redis_connection
+
+    def on_get(self, req, resp):
+        with self._redis.pipeline() as pipe:
+            pipe.get("totalRecoveredNumbers")
+            results = pipe.execute()
+
+        for item in results:
+            try:
+                check_int = int(item)
+            except Exception:
+                logger.info("invalid information.")
+                totalRecoveredNumbers = None
+                break
+
+            totalRecoveredNumbers = int(results[0])
+
+        resp.media = {
+            "data": {
+                "totalRecoveredNumbers": totalRecoveredNumbers
+            }
+        }
+    
 api = falcon.API()
 database = Database()
 
@@ -90,3 +164,6 @@ country_redis = database._countryRedis
 api.add_route("/status", Ping(redis_connection))
 api.add_route("/latest/cases", LatestCases(redis_connection, country_redis))
 api.add_route("/latest/numbers", LatestNumbers(redis_connection))
+api.add_route("/latest/numbers/confirmed", LatestConfirmed(redis_connection))
+api.add_route("/latest/numbers/deaths", LatestDeaths(redis_connection))
+api.add_route("/latest/numbers/recovered", LatestRecovered(redis_connection))
